97256d34c51c752dd86f111a09672b0a59532b9f
Constantin Catalina <catalina.constantin@student.upt.ro>
2025-07-12 14:17:36 +0300

    add volume smoothness and set volume using pinky

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,6 +1,6 @@
 import cv2
 import mediapipe as mp
-import time
+import math
 
 class handDetector():
     
@@ -17,6 +17,7 @@ class handDetector():
             min_tracking_confidence=self.trackCon
         )
         self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
 
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -29,17 +30,61 @@ class handDetector():
 
         return img
     
-    def finPosition(self, img, handNo=0, draw=True):
+    def findPosition(self, img, handNo=0, draw=True, drawBox=False):
         
-        lmList = []
+        xList = []
+        yList = []
+        bbox = []
+
+        self.lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
 
             for id, lm in enumerate(myHand.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                lmList.append([id, cx, cy])
+                xList.append(cx)
+                yList.append(cy)
+                self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
+
+            if drawBox:
+                cv2.rectangle(img, (bbox[0]-20, bbox[1]-20), (bbox[2]+20, bbox[3]+20), (0, 255, 0), 2)
+
+        return self.lmList, bbox
+
+    def fingersUp(self):
+        fingers = []
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
         
-        return lmList
+        # 4 Fingers
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        
+        return fingers
+
+    def findDistance(self, p1, p2, img, draw=True):
+        x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
+        x2, y2 = self.lmList[p2][1], self.lmList[p2][2]   
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2   
+
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -1,6 +1,5 @@
 import cv2
 import time
-import math
 import numpy as np
 import HandTrackingModule as htm
 from ctypes import cast, POINTER
@@ -20,7 +19,7 @@ cap.set(4, hCam)
 pTime = 0   # Previous time
 cTime = 0   # Current time
 
-detector = htm.handDetector(detectionCon=0.7)
+detector = htm.handDetector(detectionCon=0.7, maxHands=1)
 
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
@@ -34,43 +33,61 @@ maxVol = volRange[1]
 vol = 0
 volBar = 400
 volPer = 0
+area = 0
+colorVol = (255, 0, 0)
 
 while True:
     success, img = cap.read()
     if not success:
             break  # Exit if the frame is not captured successfully
-
+    
+    # Find Hand
     img = cv2.flip(img, 1)
     img = detector.findHands(img)  # Detect hands in the image
-    lmList = detector.finPosition(img, draw=False)  # Get the list of hand landmarks
+    lmList, bbox = detector.findPosition(img, draw=False, drawBox=True)  # Get the list of hand landmarks
     
     if len(lmList) != 0:
-        # print(lmList[4], lmList[8])
+        # Filter based on size
+        wB, hB = bbox[2]-bbox[0], bbox[3]-bbox[1]
+        area = (wB * hB) // 100
+        
+        print(area)
+        if 250 < area < 1000:
 
-        x1, y1 = lmList[4][1], lmList[4][2]
-        x2, y2 = lmList[8][1], lmList[8][2]   
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2   
+            # Find the distance between Index and Thumb
+            length, img, lineInfo = detector.findDistance(4, 8, img)
+            print(length)
 
-        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            # Convert Volume
+            volBar = np.interp(length, [10, 200], [400, 150])
+            volPer = np.interp(length, [10, 200], [0, 100])
 
-        length = math.hypot(x2 - x1, y2 - y1)
-        # print(length)
+            # Reduce Resolution to make it smoother
+            smoothness = 2
+            volPer = smoothness * round(volPer / smoothness)
 
-        vol = np.interp(length, [10, 200], [minVol, maxVol])
-        volBar = np.interp(length, [10, 200], [400, 150])
-        volPer = np.interp(length, [10, 200], [0, 100])
+            # Check which fingers are up
+            fingers = detector.fingersUp()
+            # print(fingers)
 
-        # print(vol)
-        volume.SetMasterVolumeLevel(vol, None)
+            # Check if pinky is down and set volume
+            if not fingers[4]:
+                volume.SetMasterVolumeLevelScalar(volPer / 100, None)
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                colorVol = (0, 255, 0)
+            else:
+                colorVol = (255, 0, 0)
 
+    # Drawings
     cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
     cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
     cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 
-                1, (255, 0, 0), 3)
+                    1, (255, 0, 0), 3)
 
+    cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
+    cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX, 1, colorVol, 3)
+
+    # Frame rate
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
@@ -89,4 +106,3 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()  # Close all OpenCV windows
-
